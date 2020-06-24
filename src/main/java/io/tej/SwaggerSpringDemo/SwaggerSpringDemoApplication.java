bc159ab9e9d0f43641637a0ec29b3a13cc10531f
Tejas Ozarkar <tejozarkar@gmail.com>
2020-06-24 22:56:37 +0530

    Spring boot project created

---
+++ src/main/java/io/tej/SwaggerSpringDemo/SwaggerSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package io.tej.SwaggerSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tej/SwaggerSpringDemo/SwaggerSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tej.SwaggerSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
