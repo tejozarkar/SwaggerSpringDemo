63c14d01d31f3ac385be049fb7222fa2ad3e38a8
Tejas Ozarkar <tejozarkar@gmail.com>
2020-06-25 22:17:22 +0530

    Book Controller Added

---
+++ src/main/java/io/tej/SwaggerSpringDemo/BookController.java
@@ -0,0 +1,26 @@
+package io.tej.SwaggerSpringDemo;
+
+import io.tej.SwaggerCodgen.api.BookApi;
+import io.tej.SwaggerCodgen.model.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookController implements BookApi {
+
+    @Override
+    public ResponseEntity<String> addBook(Book book) {
+        return ResponseEntity.ok(book.getTitle()+" is added");
+    }
+
+    @Override
+    public ResponseEntity<List<Book>> getBooks() {
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book().title("Name of the Wind").author("Patrick"));
+        bookList.add(new Book().title("Mistborn").author("Brandon"));
+        return ResponseEntity.ok(bookList);
+    }
+}
